8f2ce35bfb0c43e1a67c1a9a5f5856bf2f43ceb3
Drew Cook <altheawebservices@gmail.com>
2017-04-16 18:55:57 -0600

    Decompose more, created Counter component

--- app.jsx
+++ app.jsx
@@ -11,6 +11,21 @@ Header.propTypes = {
 	title: React.PropTypes.string.isRequired,
 };
 
+// Counter component - decompose the player component even further to include the counter component
+function Counter(props) {
+	return (
+		<div className="counter">
+			<button className="counter-action decrement"> - </button>
+			<div className="counter-score">{props.score}</div>
+			<button className="counter-action increment"> + </button>
+		</div>
+	);
+}
+// Counter property types
+Counter.propTypes = {
+	score: React.PropTypes.number.isRequired,
+}
+
 // Player component - so we can reuse it multiple times and make our application much more easy to read.  It is essential to break down our main application component into multiple smaller components so we are more organized and things are easier to edit.
 function Player(props) {
 	return (
@@ -19,11 +34,7 @@ function Player(props) {
 				{props.name}
 			</div>
 			<div className="player-score">
-				<div className="counter">
-					<button className="counter-action decrement"> - </button>
-					<div className="counter-score">{props.score}</div>
-					<button className="counter-action increment"> + </button>
-				</div>
+				<Counter score={props.score} />
 			</div>
 		</div>
 	);
