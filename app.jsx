1bbe355ee61ac52cf7705ba843a509720d51a32f
Drew Cook <altheawebservices@gmail.com>
2017-04-16 19:17:44 -0600

    Created data model of our players, and added a loop to iterated over that data model

--- app.jsx
+++ app.jsx
@@ -1,3 +1,33 @@
+// Lets create a data model for all of our players and give them their values
+var PLAYERS = [
+	{
+		name: "Bugs Bunny",
+		score: 48,
+		id: 1,
+	},
+	{
+		name: "Elmer Fudd",
+		score: 35,
+		id: 2,
+	},
+	{
+		name: "Daffy Duck",
+		score: 42,
+		id: 3,
+	},
+	{
+		name: "Porky Pig",
+		score: 28,
+		id: 4,
+	},
+	{
+		name: "Tweety Bird",
+		score: 37,
+		id: 5,
+	},
+];
+
+
 // Header component - we are extracting to make our applicatoin easier to read
 function Header(props) {
 	return (
@@ -47,14 +77,15 @@ Player.propTypes = {
 
 // Application component - this will be comprised of our smaller components that we've already composed.  It is much more organized to have smaller high level components within the application rather than one large beefy component with so much going on within it.  Because we have decomposed the application into smaller components, we can edit those individually later on down the road and it will be much easier to read.
 function Application(props) {
+	// We will use instances of our Header component, and Player component.  We will iterate over our array of players using the map() method, which takes a function.  The argument for our function is what we use to draw the data from each index of our array.  This is how we can loop over our data model PLAYERS.  We return it as an instance of our Player component and map each key/value pair for each one.
 	return (
 		<div className="scoreboard">
 			<Header title={props.title} />
 
 			<div className="players">
-				<Player name="Johnny Appleseed" score={53} />
-				<Player name="Paul Bunyan" score={48} />
-				<Player name="Lochness Monster" score={40} />
+				{props.players.map(function(player){
+					return <Player name={player.name} score={player.score} key={player.id} />
+				})}
 			</div>
 		</div>
 	);
@@ -63,6 +94,12 @@ function Application(props) {
 // Define proptery types - this is best practice to specify the property types in each compontent.  It will make debugging a lot easier and save a lot of time down the road.  You can make each property a required property by adding in '.isRequired'.
 Application.propTypes = {
 	title: React.PropTypes.string,
+	// We use players which is an array that is comprised of objects in the following 'shape' which includes name, score, and KEY (id) and their types
+	players: React.PropTypes.arrayOf(React.PropTypes.shape({
+		name: React.PropTypes.string.isRequired,
+		score: React.PropTypes.number.isRequired,
+		id: React.PropTypes.number.isRequired,
+	})).isRequired,
 };
 
 // Define default proptery values - this will provide a default value if they are not later specified explicitly in the instance we create in the .render() function.  It is not necessary to specify a default value for ones are are required, because they will need to be defined explicitely anyway.
@@ -71,5 +108,5 @@ Application.defaultProps = {
 };
 
 
-// Render Application component into .container div in DOM tree
-ReactDOM.render(<Application />, document.getElementById('container'));
+// Render Application component into .container div in DOM tree; include our PLAYERS data model to be used
+ReactDOM.render(<Application players={PLAYERS} />, document.getElementById('container'));
